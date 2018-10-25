3074b686119d92a359c5d6ab30228e884e4de37e
maforeroo <maforeroo@gmail.com>
2018-10-25 16:22:12 -0500

    First Commit

--- pet-clinic-web/src/main/java/com/multifx/springpetclinicmultifx/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/multifx/springpetclinicmultifx/controllers/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"" , "index" , "index.html"})
+    @RequestMapping({"" , "/", "index" , "index.html"})
     public String index(Model model){
 
         return "index";
